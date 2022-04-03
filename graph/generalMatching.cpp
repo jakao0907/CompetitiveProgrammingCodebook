8a7ef44fb509023edb953381fd097c21166204fd
jakao <47694391+jakao0907@users.noreply.github.com>
2022-04-04 02:27:05 +0800

    Update generalMatching.cpp

--- graph/generalMatching.cpp
+++ graph/generalMatching.cpp
@@ -1,11 +1,10 @@
-const int N = 514, E = (2e5) * 2;
-struct Graph{
-  int to[E],bro[E],head[N],e;
-  int lnk[N],vis[N],stp,n;
-  void init( int _n ){
-    stp = 0; e = 1; n = _n;
-    for( int i = 1 ; i <= n ; i ++ )
-      lnk[i] = vis[i] = 0;
+// should shuffle vertices and edges
+const int N=100005,E=(2e5)*2+40;
+struct Graph{ // 1-based; match: i <-> lnk[i]
+  int to[E],bro[E],head[N],e,lnk[N],vis[N],stp,n;
+  void init(int _n){
+    stp=0; e=1; n=_n;
+    for(int i=1;i<=n;i++) head[i]=lnk[i]=vis[i]=0;
   }
   void add_edge(int u,int v){
     to[e]=v,bro[e]=head[u],head[u]=e++;
@@ -15,24 +14,21 @@ struct Graph{
     vis[x]=stp;
     for(int i=head[x];i;i=bro[i]){
       int v=to[i];
-      if(!lnk[v]){
-        lnk[x]=v,lnk[v]=x;
-        return true;
-      }else if(vis[lnk[v]]<stp){
-        int w=lnk[v];
-        lnk[x]=v,lnk[v]=x,lnk[w]=0;
-        if(dfs(w)){
-          return true;
-        }
+      if(!lnk[v]){ lnk[x]=v,lnk[v]=x; return true; }
+    } 
+    for(int i=head[x];i;i=bro[i]){
+      int v=to[i];
+      if(vis[lnk[v]]<stp){
+        int w=lnk[v]; lnk[x]=v,lnk[v]=x,lnk[w]=0;
+        if(dfs(w)) return true;
         lnk[w]=v,lnk[v]=w,lnk[x]=0;
-    } }
+      }
+    }
     return false;
   }
   int solve(){
-    int ans = 0;
-    for(int i=1;i<=n;i++)
-      if(!lnk[i]){
-        stp++; ans += dfs(i);
-      }
+    int ans=0;
+    for(int i=1;i<=n;i++) if(!lnk[i]) stp++,ans+=dfs(i);
     return ans;
-} }graph;
+  }
+}graph;

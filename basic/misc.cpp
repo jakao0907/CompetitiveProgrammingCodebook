d82969cc910bca31c05a8aadd6c27c94a6996233
jakao <47694391+jakao0907@users.noreply.github.com>
2022-07-10 02:53:55 +0800

    Update misc.cpp

--- basic/misc.cpp
+++ basic/misc.cpp
@@ -2,12 +2,13 @@
 //check special cases for example (n==1)
 //check size  arrays
 
-#include <random>
 mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
 int randint(int lb, int ub)
 { return uniform_int_distribution<int>(lb, ub)(gen); }
 
 #define SECs ((double)clock() / CLOCKS_PER_SEC)
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
 struct KeyHasher {
 	size_t operator()(const Key& k) const {
@@ -19,4 +20,3 @@ __builtin_popcountll    //換成二進位有幾個1
 __builtin_clzll         //返回左起第一個1之前0的個數
 __builtin_parityll      //返回1的個數的奇偶性
 __builtin_mul_overflow(a,b,&h) //回傳a*b是否溢位
-

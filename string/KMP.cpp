9ff721a6b76fdd2973245983da2592ef395e6858
jakao0907 <47694391+jakao0907@users.noreply.github.com>
2022-10-12 22:05:14 +0800

    update KMP.cpp

--- string/KMP.cpp
+++ string/KMP.cpp
@@ -9,23 +9,20 @@ failuer[k]、failure[failuer[k]-1]
 、failure[failure[failuer[k]-1]-1]..
 直到有值為0為止 */
 int failure[MXN];
-void KMP(string& t, string& p)
-{
+vector<int> KMP(string& t, string& p){
+    vector<int> ret;
     if (p.size() > t.size()) return;
-    for (int i=1, j=failure[0]=-1; i<p.size(); ++i)
-    {
+    for (int i=1, j=failure[0]=-1; i<p.size(); ++i){
         while (j >= 0 && p[j+1] != p[i])
             j = failure[j];
         if (p[j+1] == p[i]) j++;
         failure[i] = j;
     }
-    for (int i=0, j=-1; i<t.size(); ++i)
-    {
+    for (int i=0, j=-1; i<t.size(); ++i){
         while (j >= 0 && p[j+1] != t[i])
             j = failure[j];
         if (p[j+1] == t[i]) j++;
-        if (j == p.size()-1)
-        {
-            cout << i - p.size() + 1<<" ";
+        if (j == p.size()-1){
+            ret.push_bck( i - p.size() + 1 );
             j = failure[j];
 }   }   }

1bd5e01d4989499c219a62e4bd45cbed621a3cf6
jakao <cykao0907@gmail.com>
2024-02-25 23:47:07 +0800

    update python trick

--- basic/python_trick.py
+++ basic/python_trick.py
@@ -1,16 +1,11 @@
-parser:
-int(eval(num.replace("/","//")))
+int(eval(num.replace("/","//"))) # parser string num
 
 from fractions import Fraction
 from decimal import Decimal, getcontext
-getcontext().prec = 250 # set precision
-
-itwo = Decimal(0.5)
-two = Decimal(2)
-
-format(x, '0.10f') # set precision
-
-N = 200
+getcontext().prec = 250 # set precision (MAX_PREC)
+getcontext().Emax = 250 # set exponent limit (MAX_EMAX)
+getcontext().rounding = ROUND_FLOOR # set round floor
+itwo,two,N = Decimal(0.5),Decimal(2),200
 def angle(cosT):
   """given cos(theta) in decimal return theta"""
   for i in range(N):
@@ -18,3 +13,12 @@ def angle(cosT):
   sinT = (1 - cosT * cosT) ** itwo
   return sinT * (2 ** N)
 pi = angle(Decimal(-1))
+Fraction('3.1415926535897932').limit_denominator(1000)
+
+format(x, '0.10f') # set precision
+
+x = Fraction(1, 2), y = Fraction(1)
+print(x.as_integer_ratio()) # print 1/2
+print(x.is_integer()) # return is divisible
+print(x.__round__()) 
+print(float(x)) # print 0.5

cfdbb8631f7476e47b8a6d8660a6507b2341ef41
jakao0907 <cykao0907@gmail.com>
2024-08-23 03:27:56 +0800

    cover original TLE pollard rho

--- math/pollardRho.cpp
+++ math/pollardRho.cpp
@@ -1,15 +1,11 @@
 // does not work when n is prime  O(n^(1/4))
 LL f(LL x, LL mod){ return add(mul(x,x,mod),1,mod); }
 LL pollard_rho(LL n) {
-	if(!(n&1)) return 2;
-  while(true){
-    LL y=2, x=rand()%(n-1)+1, res=1;
-    for(int sz=2; res==1; sz*=2) {
-      for(int i=0; i<sz && res<=1; i++) {
-        x = f(x, n);
-        res = __gcd(abs(x-y), n);
-      }
-      y = x;
+ 	LL c = 1, x = 0, y = 0, p = 2, q, t = 0;
+    while (t++ % 128 or gcd(p, n) == 1) {
+        if (x == y) c++, y = f(x = 2, c, n);
+        if (q = mul(p, abs(x-y), n)) p = q;
+        x = f(x, c, n); y = f(f(y, c, n), c, n);
     }
-    if (res!=0 && res!=n) return res;
-} }
+    return gcd(p, n);
+}

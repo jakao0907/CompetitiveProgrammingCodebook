aa2714e6141700dee917a2ee98ec52ccdbe0143e
jakao0907 <cykao0907@gmail.com>
2024-08-23 21:22:03 +0800

    fix check x is integer function

--- basic/python_trick.py
+++ basic/python_trick.py
@@ -19,6 +19,6 @@ format(x, '0.10f') # set precision
 
 x = Fraction(1, 2), y = Fraction(1)
 print(x.as_integer_ratio()) # print 1/2
-print(x.is_integer()) # return is divisible
+print(x.denominator == 1) # return true if x is integer
 print(x.__round__()) 
 print(float(x)) # print 0.5

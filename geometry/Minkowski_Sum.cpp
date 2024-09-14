d47ba6bf2700c73fadb21cfb924fa90ba3dedd42
jakao0907 <cykao0907@gmail.com>
2024-09-14 17:02:42 +0800

    fix minkowski sum bug

--- geometry/Minkowski_Sum.cpp
+++ geometry/Minkowski_Sum.cpp
@@ -7,9 +7,10 @@ vector<Pt> minkowski(vector<Pt> P, vector<Pt> Q) {
         rotate(R.begin(), min_element(all(R), cmp), R.end());
         R.push_back(R[0]), R.push_back(R[1]);
     };
+    const int n = P.size(), m = Q.size();
     reorder(P), reorder(Q);
     vector<Pt> R;
-    for (int i = 0, j = 0, s; i < P.size() || j < Q.size(); ) {
+    for (int i = 0, j = 0, s; i < n || j < m; ) {
         R.push_back(P[i] + Q[j]);
         s = dcmp((P[i + 1] - P[i]) ^ (Q[j + 1] - Q[j]));
         if (s >= 0) i++;
